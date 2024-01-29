4949636ae286e02d65f1a843bb443097e8d9b987
Пехтелева Дарья Юрьевна <pexteleva_d_qschool@qsoft.ru>
2024-01-29 13:19:51 +0300

    Создана структура стилизации страниц, подключен препроцессор SASS, подключены базовые стили для страницы /auth

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,7 +1,44 @@
+import styles from './styles.module.scss'
+import { Input, Button } from '@mantine/core'
+import Link from 'next/link'
+
 export default function AuthPage() {
   return (
     <>
-      <h1>Страница авторизации</h1>
+      <div className={styles.authWrapper}>
+        <div className={styles.auth}>
+          <h1 className={styles.authTitle}>Страница авторизации</h1>
+
+          <div className={styles.authContent}>
+            <div className={styles.authContentWrapper}>
+              <Input
+                className={styles.authInput}
+                variant="unstyled"
+                size="xl"
+                placeholder="Ввведите имя"
+              />
+              <Input
+                className={styles.authInput}
+                variant="unstyled"
+                size="xl"
+                placeholder="Ввведите идентификатор лобби"
+              />
+
+              <Button
+                className={styles.authButton}
+                variant="filled"
+                color="cyan"
+                fullWidth
+                size="lg"
+              >
+                Прыгнуть в игру
+              </Button>
+
+              <div>Или <Link href="/lobby" className={styles.authLink}>создайте лобби</Link></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>      
   );
 };
