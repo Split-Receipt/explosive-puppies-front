05b8166a690b91d048665e78f32a9b876bb0a975
Федор <1fedia-volkov@mail.ru>
2024-02-01 16:31:58 +0300

    Создан компонент игрока, подключены стили для игрока

--- src/app/lobby/page.tsx
+++ src/app/lobby/page.tsx
@@ -1,7 +1,12 @@
+import styles from './styles.module.scss'
+import PlayerProfileCard from '../components/player-profile-card/player-profile-card'
+
 export default function LobbyPage() {
   return (
     <>
       <h1>Страница лобби</h1>
+      <PlayerProfileCard></PlayerProfileCard>
+
     </>      
   );
 };
