41ad4a2104e45a45ecf469354042629dc5f9834d
Федор <1fedia-volkov@mail.ru>
2024-02-14 14:27:06 +0300

    Список игроков обновляется при получении новых игроков, добавлено вертикальное и горизонтальное расположение карточек игроков

--- src/app/components/list-of-players/list-of-players.tsx
+++ src/app/components/list-of-players/list-of-players.tsx
@@ -1,11 +1,39 @@
+'use client'
 import styles from './list-of-players.module.scss'
 import PlayerProfileCard from '../player-profile-card/player-profile-card';
+import { useMemo } from 'react';
 
-export default function ListOfPlayers({player}) {
+const defaultPlayersList = [
+  {userName: 'Player #1', id: '1', avatar: '#'},
+  {userName: 'Player #2', id: '2', avatar: '#'},
+  {userName: 'Player #3', id: '3', avatar: '#'},
+  {userName: 'Player #4', id: '4', avatar: '#'},
+  {userName: 'Player #5', id: '5', avatar: '#'},
+  {userName: 'Player #6', id: '6', avatar: '#'}
+]
+const replaceDefaults = (defaultPlayersList, newPlayersList) => {
+  let result = []
+  if(!newPlayersList.length) {
+      result = defaultPlayersList
+  } else {
+    for(let iterator = 0; iterator < 6; iterator++){
+      const playerInNewPlayersList = newPlayersList[iterator]
+      if(playerInNewPlayersList){
+        result[iterator] = playerInNewPlayersList
+      } else {
+        result[iterator] = defaultPlayersList[iterator]
+      }
+    }
+  }
+  
+  return result
+}
+export default function ListOfPlayers({players}) {
+ const actualPlayersList = useMemo(() => replaceDefaults(defaultPlayersList, players),[defaultPlayersList, players])
     return (
       <>
       <div className={styles.players}>
-       {player.map((player) => <PlayerProfileCard id={player.id} userName={player.userName} avatar={player.avatar} key={player.id}></PlayerProfileCard>)}      
+       {actualPlayersList.map((player) => <PlayerProfileCard id={player.id} userName={player.userName} avatar={player.avatar} key={player.id} position='horizontal'></PlayerProfileCard>)}      
       </div>
       </>      
     );
--- src/app/lobby/page.tsx
+++ src/app/lobby/page.tsx
@@ -3,17 +3,14 @@ import ListOfPlayers from '../components/list-of-players/list-of-players'
 
 export default function LobbyPage() {
   const players = [
-    {userName: 'Player #1', id: '1', avatar: '#'},
-    {userName: 'Player #2', id: '2', avatar: '#'},
-    {userName: 'Player #3', id: '3', avatar: '#'},
-    {userName: 'Player #4', id: '4', avatar: '#'},
-    {userName: 'Player #5', id: '5', avatar: '#'},
-    {userName: 'Player #6', id: '6', avatar: '#'}
+    {userName: 'Player #4', id: '15', avatar: '#'},
+    {userName: 'Player #5', id: '25', avatar: '#'},
+    {userName: 'Player #6', id: '555', avatar: '#'}
   ]
   return (
     <>
       <h1>Страница лобби</h1>
-      <div className={styles.lobbyPlayers}><ListOfPlayers player={players}></ListOfPlayers></div>
+      <div className={styles.lobbyPlayers}><ListOfPlayers players={players}></ListOfPlayers></div>
     </>      
   );
 };
